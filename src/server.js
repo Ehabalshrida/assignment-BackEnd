7ddef818d8a2463227a8e4a07e7ef25452d319b8
Ehabalshrida <ehabshridah@gmail.com>
2022-04-02 17:34:52 +0300

    solved

---
+++ src/server.js
@@ -0,0 +1,21 @@
+'use strict'
+const express = require('express');
+require('dotenv').config();
+const router = require("./routes/rout");
+const app = express();
+const PORT = process.env.PORT
+app.use(express.json());
+const cors = require('cors');
+app.use(cors())
+function start() {
+    app.listen(PORT, () => { console.log(`listning to ${PORT}`); })
+}
+
+app.get('/',(req,res)=>{res.send('hello from /')})
+
+app.use(router)
+
+module.exports = {
+    start,
+    app
+}
